82e26277abbb392f207fa309b1c188b20cfb8c62
daniel kassampu tshimanga <102564408+dtshimanga14@users.noreply.github.com>
2022-08-31 23:57:05 -0600

    create Post, Feed, App components

--- wb-front-end/src/App.js
+++ wb-front-end/src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import Feed from "./Feed";
+import Post from "./Post";
 
 function App() {
+  const feeds = [
+    { src : "/pics/black-family.jpg"},
+    { src : "/pics/hawai.webp"},
+];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Here will be the header</header>
+      {feeds.map((feed) => (<Post d ={feed}/>))}
     </div>
   );
 }
---
+++ wb-front-end/src/Post.js
@@ -0,0 +1,13 @@
+import "./css/Post.css";
+
+const Post = ({ d }) => {
+    return (
+      <div>
+        <img 
+          className = "" src={d.src}
+          alt="here must be an"
+        />
+    </div>
+    );
+};
+export default Post;
