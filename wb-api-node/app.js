49b2cd8a59e327b54b836603a9cb6a24636411c3
daniel kassampu tshimanga <102564408+dtshimanga14@users.noreply.github.com>
2022-08-31 22:53:17 -0600

    node apis adding connection with mongo-atlas

--- wb-api-node/app.js
+++ wb-api-node/app.js
@@ -1,9 +1,27 @@
-const express = require("express");
-var app = express();
+require("dotenv").config();
 
-app.get("/",(req,res) => {
-    res.json({ message : "hello world " });
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const crypto = require("crypto");
+const mongoose = require("mongoose");
+const multer = require("multer");
+const { GridFsStorage } = require("multer-gridfs-storage");
+const Grid = require('gridfs-stream');
+
+const mongoURI = "mongodb+srv://root:2YKasj80X1oJHFSV@wb-db.dongv5t.mongodb.net/?retryWrites=true&w=majority";
+
+
+var app = express();
+const client = new MongoClient(mongoURI);
+
+app.get("/users",async (req,res) => {
+    await client.connect();
+    const database = await client.db("whiteblue");
+    const users = database.collection('users');
+    const user = await users.findOne()
+    console.log(user);
+    res.json(user);
 })
-.listen(9000,()=> {
+.listen(process.env.PORT,()=> {
     console.log("wb-node-apis is running on 9000");
 });
