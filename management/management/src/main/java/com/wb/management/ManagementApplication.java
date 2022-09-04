0187045e72bde61ac0f79883974e2189980735d8
daniel kassampu tshimanga <102564408+dtshimanga14@users.noreply.github.com>
2022-09-04 15:43:00 -0600

    management APIs integrated

---
+++ management/management/src/main/java/com/wb/management/ManagementApplication.java
@@ -0,0 +1,18 @@
+package com.wb.management;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementApplication implements CommandLineRunner  {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("hello world");
+	}
+}
